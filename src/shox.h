da4728ac386186f361e3d6b1d298e284cb6f03cc
foxlollato <vitor.lollato@gmail.com>
2017-09-21 16:20:55 -0300

    21091620

--- src/shox.h
+++ src/shox.h
@@ -7,6 +7,11 @@
 
 void parse (char*, char**);
 void welcome(pid_t);
+void runCommand(char*, char**);
+void checkCommand(char*, char**);
+//runInCommand(command,args,i); 
+//runOutCommand(command,args,i);
+//runPipeCommand(command,args,i);
 void runShox();
 
 #endif
