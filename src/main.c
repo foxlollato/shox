a98a1620c4746676b1cb90185cd1d39c75a4de75
foxlollato <vitor.lollato@gmail.com>
2017-09-21 16:29:02 -0300

    21091628

--- src/main.c
+++ src/main.c
@@ -1,17 +1,12 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-#include <pwd.h>
-
 #include "shox.h"
 
 int main (int argc, char *argv[]){
 	
 	welcome(getpid());
 
-	runShox(); //calls shell
+	runShox();
 
 
 	return EXIT_SUCCESS;
